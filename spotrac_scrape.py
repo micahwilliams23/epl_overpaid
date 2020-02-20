007483081e6ab041470dbd59b8f2cf0c9a6427ab
micahwilliams23 <micahwilliams@college.harvard.edu>
2020-02-19 23:03:09 -0500

    spotrac_scrape: added csv writer

--- spotrac_scrape.py
+++ spotrac_scrape.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup as soup
 import requests as req
 import csv
 
+with open('epl_data.csv') as f:
+    writer = csv.writer(f)
+    writer.writerow('player','position','age','contract_value','contract_length','transfer_fee')
+
 def clean_money(dirty_money):
 
     return(dirty_money.replace('£','').replace(',',''))
@@ -15,27 +19,29 @@ page_soup = soup(page_html, 'html.parser')
 rows = page_soup.findAll('tr')[1:]
 
 for player_row in rows:
+
+    row_td = player_row.findAll('td')
         
     # scrape player's name
-    player = player_row.findAll('td')[0].a.text
+    player = row_td[0].a.text
 
     # scrape player's position
-    position = player_row.findAll('td')[1].text
+    position = row_td[1].text
 
     # scrape player's age
-    age = player_row.findAll('td')[2].text
+    age = row_td[2].text
 
-    # scrape total contract value, then clean away '£' and ','
-    contract_value_dirty = player_row.findAll('td')[3].span[0].text
-    contract_value = clean_money(contract_value_dirty)
+    # scrape total contract value
+    contract_value = row_td[3].span.text
 
     # scrape contract length
-    contract_length = player_row.findAll('td')[3].span[1].text
+    contract_length = row_td[3].findAll('span')[1].text
 
     # scrape transfer fee, then clean away '£' and ','
-    transfer_fee_dirty = player_row.findAll('td')[5].span[0].text
+    transfer_fee_dirty = row_td[5].span.text
+
     transfer_fee = clean_money(transfer_fee_dirty)
 
-    print(player, age)
-
-    exit(1)
+    with open('epl_data.csv') as f:
+        writer = csv.writer(f)
+        writer.writerow(player, position, age, contract_value, contract_length, transfer_fee)
