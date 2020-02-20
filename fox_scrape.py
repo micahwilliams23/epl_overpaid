c9b3df9b1d873cf5a717a08f078003348208f117
micahwilliams23 <micahwilliams@college.harvard.edu>
2020-02-20 10:58:05 -0500

    fox_scrape: added code to scrape player name

--- fox_scrape.py
+++ fox_scrape.py
@@ -1,2 +1,17 @@
 from bs4 import BeautifulSoup as soup
 import requests as req
+import csv
+
+test_url = 'https://www.foxsports.com/soccer/stats?competition=1&season=20190&category=standard&sort=3'
+
+page_html = req.get(test_url).content
+
+page_soup = soup(page_html, 'html.parser')
+
+player_row = page_soup.findAll('table',{'class':'wisbb_standardTable'})[0].tbody
+
+player = player_row.a.span.text
+
+games = player_row.tr.td
+
+print(games)
