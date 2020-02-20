45a9c609de9b783454e6ba20e93d29471ed51093
micahwilliams23 <micahwilliams@college.harvard.edu>
2020-02-20 12:42:08 -0500

    successfully scraped player stats

--- fox_scrape.py
+++ fox_scrape.py
@@ -8,30 +8,35 @@ with open('epl_stats.csv', 'w', newline = '') as f:
     writer = csv.writer(f)
     writer.writerow(header)
 
-test_url = 'https://www.foxsports.com/soccer/stats?competition=1&season=20190&category=standard&sort=3'
+url_root = 'https://www.foxsports.com/soccer/stats?competition=1&season=20190&category=STANDARD&pos=0&team=0&isOpp=0&sort=3&sortOrder=0&page='
 
-page_html = req.get(test_url).content
+for i in range(1,11):
 
-page_soup = soup(page_html, 'html.parser')
+    page_html = req.get(url_root+str(i)).content
 
-player_rows = page_soup.findAll('table',{'class':'wisbb_standardTable'}).tbody
+    page_soup = soup(page_html, 'html.parser')
 
-for player_row in player_rows:
+    player_table = page_soup.findAll('table',{'class':'wisbb_standardTable'})[0].tbody
 
-    player = player_row.a.span.text
+    player_rows = player_table.findAll('tr')
 
-    player_data = player_row.tr.findAll('td')
-    games = player_data[1].text
-    starts = player_data[2].text
-    goals = player_data[4].text
-    assists = player_data[5].text
-    shots_on_goal = player_data[6].text
-    shots = player_data[7].text
-    yellow = player_data[8].text
-    red = player_data[9].text
+    for player_row in player_rows:
 
-    newrow = [player, games, starts, goals, assists, shots_on_goal, shots, yellow, red]
+        player = player_row.a.span.text
+        print('NAME: ' + player)
+        player_data = player_row.findAll('td')
 
-    with open('epl_stats.csv', 'a', newline = '') as f:
-        writer = csv.writer(f)
-        writer.writerow(newrow)
+        games = player_data[1].text
+        starts = player_data[2].text
+        goals = player_data[4].text
+        assists = player_data[5].text
+        shots_on_goal = player_data[6].text
+        shots = player_data[7].text
+        yellow = player_data[8].text
+        red = player_data[9].text
+
+        newrow = [player, games, starts, goals, assists, shots_on_goal, shots, yellow, red]
+
+        with open('epl_stats.csv', 'a', newline = '') as f:
+            writer = csv.writer(f)
+            writer.writerow(newrow)
