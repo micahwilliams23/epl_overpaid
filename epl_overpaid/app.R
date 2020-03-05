1c0e6c8888f640131350b78fa1b2193c89819334
micahwilliams23 <micahwilliams@college.harvard.edu>
2020-03-05 11:52:11 -0500

    added graph of team's top-paid players

---
+++ epl_overpaid/app.R
@@ -0,0 +1,65 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- navbarPage(
+    "Final Project Title",
+    tabPanel("Model",
+             fluidPage(
+                 titlePanel("Model Title"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput(
+                             "plot_type",
+                             "Plot Type",
+                             c("Option A" = "a", "Option B" = "b")
+                         )),
+                     mainPanel(plotOutput("line_plot")))
+             )),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is where I talk about my project."),
+             h3("About Me"),
+             p("My name is ______ and I study ______. 
+             You can reach me at ______@college.harvard.edu.")))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$line_plot <- renderPlot({
+        # Generate type based on input$plot_type from ui
+        
+        ifelse(
+            input$plot_type == "a",
+            
+            # If input$plot_type is "a", plot histogram of "waiting" column 
+            # from the faithful dataframe
+            
+            x   <- faithful[, 2],
+            
+            # If input$plot_type is "b", plot histogram of "eruptions" column
+            # from the faithful dataframe
+            
+            x   <- faithful[, 1]
+        )
+        
+        # Draw the histogram with the specified number of bins
+        
+        hist(x, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
